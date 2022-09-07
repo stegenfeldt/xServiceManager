a4d229cb23066a3ae458e0acace36609c060b764
Samuel Tegenfeldt <samuel.tegenfeldt@atea.se>
2022-09-07 10:01:59 +0200

    tests adapted to v4+

--- module/test/xServiceManager.Tests.ps1
+++ module/test/xServiceManager.Tests.ps1
@@ -1,17 +1,19 @@
-$ModuleManifestName = 'xServiceManager.psd1'
-$ModuleManifestPath = "$PSScriptRoot\..\$ModuleManifestName"
 
 Describe 'Module Manifest Tests' {
     It 'Passes Test-ModuleManifest' {
-        Test-ModuleManifest -Path $ModuleManifestPath | Should Not BeNullOrEmpty
-        $? | Should Be $true
+        $ModuleManifestName = 'xServiceManager.psd1'
+        $ModuleManifestPath = "$PSScriptRoot\..\$ModuleManifestName"    
+        Test-ModuleManifest -Path $ModuleManifestPath | Should -Not -BeNullOrEmpty
+        $? | Should -Be $true
     }
 }
 
 Describe 'Loading Module Tests' {
     It 'Loads xServiceManager module' {
-        Import-Module -$ModuleManifestPath
+        $ModuleManifestName = 'xServiceManager.psd1'
+        $ModuleManifestPath = "$PSScriptRoot\..\$ModuleManifestName"
+        Import-Module -Name $ModuleManifestPath
         $moduleLoaded = Get-Module -Name 'xServiceManager'
-        $moduleLoaded | Should Be $true
+        $moduleLoaded | Should -Be $true
     }
 }
